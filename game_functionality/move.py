26b15a8a737311e2ba48ef9e50de265d04b1d712
fotios.kapotos <fotios.kapotos@student-cs.fr>
2023-06-14 16:28:34 +0200

    BitVector Implementation completed

--- game_functionality/move.py
+++ game_functionality/move.py
@@ -7,11 +7,22 @@ class Move:
         self.piece = initial_square.piece
 
     def __str__(self):
-        s = ''
-        s += f'({self.initial.col}, {self.initial.row})'
-        s += f' -> ({self.final.col}, {self.final.row})'
+        s = self.piece.color + ' '
+        s += f'({self.initial_square.col}, {self.initial_square.row})'
+        s += f' -> ({self.final_square.col}, {self.final_square.row})'
         return s
 
+    def __repr__(self):
+        color = self.piece.color
+        init_coord = self.initial_square.single_coord()
+        final_coord = self.final_square.single_coord()
+
+        list = [0, 0, 0] if color == 'white' else [1, 1, 1]
+
+        list += ['1' if init_coord == i else '0' for i in range(9)]
+        list += ['1' if final_coord == i else '0' for i in range(9)]
+
+        return ''.join([str(i) for i in list])
 
     def __eq__(self, other):
         return self.initial_square == other.initial_square and self.final_square == other.final_square
--- game_functionality/square.py
+++ game_functionality/square.py
@@ -7,6 +7,10 @@ class Square:
 
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
+    
+    def single_coord(self):
+        return self.row * 3 + self.col
+
 
     def has_piece(self):
         return self.piece != None
