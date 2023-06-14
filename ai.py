40c8c4988e5be84ff4398a68d9d3cd2a2a50bd87
fotios.kapotos <fotios.kapotos@student-cs.fr>
2023-06-14 15:35:04 +0200

    added repr functionality

--- ai.py
+++ ai.py
@@ -6,6 +6,9 @@ class AI():
     def __init__(self):
         self.name = "Random AI"
 
+    def __repr__(self):
+        return self.name
+
 
     def get_move(self, board, color="black"):
         moves = board.all_moves(color)
